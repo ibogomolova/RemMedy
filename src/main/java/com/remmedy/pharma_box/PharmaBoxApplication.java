c675e89f5a9d16e22f0b78e6ae81ee8e90ecdccb
bogomolova <ibogomolllova@gmail.com>
2025-07-22 22:34:07 +0300

    initial commit

---
+++ src/main/java/com/remmedy/pharma_box/PharmaBoxApplication.java
@@ -0,0 +1,13 @@
+package com.remmedy.pharma_box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaBoxApplication.class, args);
+	}
+
+}
