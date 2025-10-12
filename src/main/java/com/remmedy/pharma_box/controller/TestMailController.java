c5c7b5c33d79d5e487777c9873e65aa55807cf1c
bogomolova <ibogomolllova@gmail.com>
2025-10-12 15:32:35 +0300

    fix: mail, add: properties in application.properties

--- src/main/java/com/remmedy/pharma_box/controller/TestMailController.java
+++ src/main/java/com/remmedy/pharma_box/controller/TestMailController.java
@@ -1,29 +1,32 @@
 package com.remmedy.pharma_box.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestMailController {
 
+    @Value("${spring.mail.username}")
+    private String from;
+
     @Autowired
     private JavaMailSender mailSender;
 
     @GetMapping("/test-email")
-    public String sendTestEmail() {
+    public String sendTestEmail(@RequestParam String to) {
         try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom("noreply@remmedy.site");
-            message.setTo("ibogomolllova@gmail.com");
-            message.setSubject("Проверка SMTP");
-            message.setText("Это тестовое письмо из приложения RemMedy.");
-
-            mailSender.send(message);
-
-            return "Письмо отправлено!";
+            var msg = new SimpleMailMessage();
+            msg.setFrom(from);
+            msg.setTo(to);
+            msg.setSubject("Проверка SMTP2");
+            msg.setText("Это тестовое письмо из приложения RemMedy.");
+            mailSender.send(msg);
+            return "Письмо принято SMTP.";
         } catch (Exception e) {
             e.printStackTrace();
             return "Ошибка при отправке: " + e.getMessage();
